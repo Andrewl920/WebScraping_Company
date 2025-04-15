6044838cbce69e0fbd2ea334d18822571d76535e
Andrew <andrewleung920@gmail.com>
2025-04-15 11:38:10 +1000

    get the phone number

--- Testing.py
+++ Testing.py
@@ -6,5 +6,10 @@ driver.get("https://www.yellowpages.com.au/search/listings?clue=air+conditioning
 html = driver.page_source
 soup = BeautifulSoup(html, 'html.parser')
 
+#get the name of the company
 for title in soup.find_all("h3"):
     print(title.get_text(strip=True))
+
+#get the number of the compnay
+for numbers in soup.find_all("a", "MuiButtonBase-root MuiButton-root MuiButton-text ButtonPhone wobble-call MuiButton-textSecondary MuiButton-fullWidth"):
+    print(numbers.get_text(strip= True))
