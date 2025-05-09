a2acea712c105c49987efc71e3d8fbf663d8aa39
Andrew <andrewleung920@gmail.com>
2025-05-09 15:16:58 +1000

    updated excel

--- Web_Scraping_Selenium.py
+++ Web_Scraping_Selenium.py
@@ -7,7 +7,7 @@ driver = None
 
 def init(page_number):
     global driver
-    url = "https://www.yellowpages.com.au/search/listings?clue=Electrican&locationClue=ACT"
+    url = "https://www.yellowpages.com.au/search/listings?clue=Electrican&locationClue=NT"
 
     if page_number == 1:
         url = url
