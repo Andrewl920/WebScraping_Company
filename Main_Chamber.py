4c041be4bfc0c913bacf6ce13a4bef811bd40db7
Andrew <andrewleung920@gmail.com>
2025-05-27 13:02:56 +1000

    added web scrapping file of Chamber

---
+++ Main_Chamber.py
@@ -0,0 +1,37 @@
+import Web_Scrapping_chamber as Webscraping
+from Excel import Excel
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+import time
+
+major_city = ["Sydney", "Newcastle", "Wollongong", "Melbourne", "Geelong", "Ballarat", "Bendigo", "Brisbane", "Gold Coast", "Sunshine Coast", "Townsville", "Cairns", "Toowoomba", "Mackay", "Rockhampton", "Perth", "Bunbury", "Geraldton", "Adelaide", "Mount Gambier", "Hobart", "Launceston", "Devonport", "Burnie", "Canberra", "Darwin", "Alice Springs"]
+search_type = ["Air Conditioning", "Electrican"]
+
+if __name__ == "__main__":
+    
+    for city in major_city:
+        for type in search_type:
+            company_info = []
+            company_list = {}
+
+            scraping = Webscraping.WebScraping_chamber()
+            scraping.find_type_input(type)
+            scraping.find_location_input(city)
+            total_business_card = scraping.find_business_card()
+            for card in range(len(total_business_card)):
+                total_business_card[card].click()
+
+                time.sleep(10)
+                ActionChains(scraping.driver).send_keys(Keys.ESCAPE).perform()
+
+                name = scraping.get_company_name()
+                print(name)
+                # time.sleep(1000)
+                address = scraping.get_address()
+                phone_number = scraping.get_phone_number()
+                website = scraping.get_website()
+
+                print(name, address, phone_number, website)
+                tabs = scraping.driver.window_hanldes
+                scraping.driver.close()
+
---
+++ Web_Scrapping_chamber.py
@@ -0,0 +1,85 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import undetected_chromedriver as uc
+# import undetected_chromedriver as uc
+
+class WebScraping_chamber:
+    def __init__(self):
+        self.options = uc.ChromeOptions()
+        self.options.headless = False
+        self.driver = uc.Chrome(options=self.options)
+        self.url = "https://australia.chamberofcommerce.com/business-directory/queensland/kawana/air-conditioning-contractor/3425459-jms-group-australia-refrigeration-air-conditioning-electrical"
+        self.driver.execute_cdp_cmd("Network.setBlockedURLs", {"urls": ["*doubleclick.net*", "*ads.google.com*"]})
+        self.driver.get(self.url)
+
+    def find_type_input(self, business_type):
+        self.driver.implicitly_wait(5)
+        type_input = self.driver.find_element(By.ID, "search3")
+        type_input.clear()
+        text_to_type = business_type
+        for char in text_to_type:
+            type_input.send_keys(char)
+            time.sleep(0.2)
+        
+
+    def find_location_input(self, location):
+        self.driver.implicitly_wait(5)
+        type_input = self.driver.find_element(By.ID, "citystate3")
+        type_input.clear()
+        text_to_type = location
+        for char in text_to_type:
+            type_input.send_keys(char)
+            time.sleep(0.2) 
+
+        type_input.send_keys(Keys.RETURN)
+        time.sleep(5) 
+
+    def find_business_card(self):
+        time.sleep(10) 
+        self.driver.implicitly_wait(5)
+        business_card = self.driver.find_elements(By.XPATH, '//a[@target="_blank"]')
+        return business_card
+    
+    def get_company_name(self):
+        company_name = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, "h1")))
+        return company_name.text
+
+    def get_address(self):
+        city = self.driver.find_element(By.XPATH, '//span[@selector-type="City"]')
+        state = self.driver.find_element(By.XPATH, '//span[@selector-type="State"]')
+        post_code = self.driver.find_element(By.XPATH, '//span[@selector-type="Zip"]')
+        return city.text[:-1] + " " + state.text + " " + post_code.text
+    
+    def get_phone_number(self):
+        phone_number = self.driver.find_element(By.XPATH, '//a[@selector-type="Phone"]')
+        return phone_number.text
+    
+    def get_website(self):
+        container = self.driver.find_elements(By.CLASS_NAME, "card-body")[3]
+        sentense = container.find_elements(By.CLASS_NAME, "text-dark")[5].text
+        seperate_text = sentense.split(": ")
+        website = seperate_text[1]
+        return website
+    
+    def click_next_page(self):
+        next_page_button = self.driver.find_elements(By.CLASS_NAME, "page-link")[-1]
+        return next_page_button
+    
+    
+
+
+if __name__ == "__main__":
+    web_scraping_chamber = WebScraping_chamber()
+    # web_scraping_chamber.find_type_input("Air Conditioning")
+    # web_scraping_chamber.find_location_input("Brisbane")
+    # print(web_scraping_chamber.find_business_card())
+    print(web_scraping_chamber.get_company_name())
+    print(web_scraping_chamber.get_address())
+    print(web_scraping_chamber.get_phone_number())
+    web_scraping_chamber.get_website()
+        
